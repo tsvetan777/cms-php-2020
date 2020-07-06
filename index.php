24e23d5c557ebc5f1272d464f2711c2cbc1317ef
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-06 20:32:08 +0300

    Create basic index.php template for displaying blog posts. Style according to the development specification.

---
+++ index.php
@@ -0,0 +1,76 @@
+<html>
+    <head>
+        <title>title</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    
+    <body>
+        <div id="header">
+            <h1>@CMS</h1>
+        </div>
+        
+        <div id="content">
+            
+            <ul id="category-placeholder">
+                <li class="category-placeholder--category">Клюки</li>
+                <li class="category-placeholder--category">Политика</li>
+                <li class="category-placeholder--category">Програмиране</li>
+            </ul>
+            
+            <div id="blog-post--content">
+                
+                <div class="post">
+                    <span class="post-title">Последни клюки</span>
+                    <div class="post-content">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus iaculis condimentum. Aliquam vulputate pretium mauris a convallis. 
+                            Cras faucibus odio quis nibh condimentum, id iaculis neque fermentum. Ut et elit tortor. In hac habitasse platea dictumst. Morbi 
+                            egestas nec massa in elementum. Nulla a ipsum semper, varius nulla eget, tincidunt dui. Vivamus venenatis purus augue, non consectetur 
+                            risus ornare ac. Nam dictum aliquam tellus in interdum. Proin purus est, tincidunt ac dui ac, euismod tempor nibh. Nullam sit amet 
+                            tempor justo. Integer tempus turpis in magna tincidunt pellentesque ac in nisi. Nunc viverra nunc ut maximus consequat.
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="post">
+                    <span class="post-title">Политика служба тика</span>
+                    <div class="post-content">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus iaculis condimentum. Aliquam vulputate pretium mauris a convallis. 
+                            Cras faucibus odio quis nibh condimentum, id iaculis neque fermentum. Ut et elit tortor. In hac habitasse platea dictumst. Morbi 
+                            egestas nec massa in elementum. Nulla a ipsum semper, varius nulla eget, tincidunt dui. Vivamus venenatis purus augue, non consectetur 
+                            risus ornare ac. Nam dictum aliquam tellus in interdum. Proin purus est, tincidunt ac dui ac, euismod tempor nibh. Nullam sit amet 
+                            tempor justo. Integer tempus turpis in magna tincidunt pellentesque ac in nisi. Nunc viverra nunc ut maximus consequat.
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="post">
+                    <span class="post-title">Сашо Роман президент</span>
+                    <div class="post-content">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus iaculis condimentum. Aliquam vulputate pretium mauris a convallis. 
+                            Cras faucibus odio quis nibh condimentum, id iaculis neque fermentum. Ut et elit tortor. In hac habitasse platea dictumst. Morbi 
+                            egestas nec massa in elementum. Nulla a ipsum semper, varius nulla eget, tincidunt dui. Vivamus venenatis purus augue, non consectetur 
+                            risus ornare ac. Nam dictum aliquam tellus in interdum. Proin purus est, tincidunt ac dui ac, euismod tempor nibh. Nullam sit amet 
+                            tempor justo. Integer tempus turpis in magna tincidunt pellentesque ac in nisi. Nunc viverra nunc ut maximus consequat.
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="post">
+                    <span class="post-title">Излезе HTML8</span>
+                    <div class="post-content">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus iaculis condimentum. Aliquam vulputate pretium mauris a convallis. 
+                            Cras faucibus odio quis nibh condimentum, id iaculis neque fermentum. Ut et elit tortor. In hac habitasse platea dictumst. Morbi 
+                            egestas nec massa in elementum. Nulla a ipsum semper, varius nulla eget, tincidunt dui. Vivamus venenatis purus augue, non consectetur 
+                            risus ornare ac. Nam dictum aliquam tellus in interdum. Proin purus est, tincidunt ac dui ac, euismod tempor nibh. Nullam sit amet 
+                            tempor justo. Integer tempus turpis in magna tincidunt pellentesque ac in nisi. Nunc viverra nunc ut maximus consequat.
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <div id="footer"></div>
+    </body>
+</html>
