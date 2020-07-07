0ca56de2ffc6ba8919cf235058c843c2889cc82c
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-07 20:07:55 +0300

    Add new table post migration

--- sql/migration.sql
+++ sql/migration.sql
@@ -42,10 +42,18 @@ INSERT INTO mycms.tb_blogpost (title, content, preview_content)
 			risus ornare ac. Nam dictum aliquam tellus in interdum. Proin purus est, tincidunt ac dui ac, euismod tempor nibh. Nullam sit amet 
 			tempor justo. Integer tempus turpis in magna tincidunt pellentesque ac in nisi. Nunc viverra nunc ut maximus consequat.',
             'Невероятно, но факт');
+
+INSERT INTO mycms.tb_blogpost (title, content, preview_content)
+	VALUES('Започнахме да пишем бази', 
+		   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus iaculis condimentum. Aliquam vulputate pretium mauris a convallis. 
+			Cras faucibus odio quis nibh condimentum, id iaculis neque fermentum. Ut et elit tortor. In hac habitasse platea dictumst. Morbi 
+			egestas nec massa in elementum. Nulla a ipsum semper, varius nulla eget, tincidunt dui. Vivamus venenatis purus augue, non consectetur 
+			risus ornare ac. Nam dictum aliquam tellus in interdum. Proin purus est, tincidunt ac dui ac, euismod tempor nibh. Nullam sit amet 
+			tempor justo. Integer tempus turpis in magna tincidunt pellentesque ac in nisi. Nunc viverra nunc ut maximus consequat.',
+            'Ами, започнахме');
             
             
             
             
             
-            
-            
+              
