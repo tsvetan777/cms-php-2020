036a3f2075dc8ac37efb1af7de7ebc18cc7a71ce
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-10 19:36:01 +0300

    Create general template files containing basic application logic

---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<?php include './src/db/database.php'; ?>
+
+<html>
+    <head>
+        <title>title</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    
+    <body>
+        <div id="header">
+            <h1>@CMS</h1>
+        </div>
+        
+        <div id="content">
