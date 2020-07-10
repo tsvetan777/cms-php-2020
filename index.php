ed792a384dfc93d8d7a8903d3a951b832fe49bb0
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-10 19:38:42 +0300

    Applied common logic into template file

--- index.php
+++ index.php
@@ -1,54 +1,32 @@
-<?php include './src/db/database.php'; ?>
-<?php 
+<?php include ('./templates/header.php'); ?>
 
-    $mysqlResult = query("SELECT * FROM tb_blogpost");
-    
-?>
-
-<html>
-    <head>
-        <title>title</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    
-    <body>
-        <div id="header">
-            <h1>@CMS</h1>
-        </div>
-        
-        <div id="content">
+<ul id="category-placeholder">
+    <li class="category-placeholder--category">Клюки</li>
+    <li class="category-placeholder--category">Политика</li>
+    <li class="category-placeholder--category">Програмиране</li>
+</ul>
             
-            <ul id="category-placeholder">
-                <li class="category-placeholder--category">Клюки</li>
-                <li class="category-placeholder--category">Политика</li>
-                <li class="category-placeholder--category">Програмиране</li>
-            </ul>
-            
-            <div id="blog-post--content">
+<div id="blog-post--content">
                 
-                <div class="post">
-                    <?php 
+    <div class="post">
+        <?php 
                     
-                    $mysqlResult = query("SELECT * FROM tb_blogpost");
-                    while($blogPost = mysqli_fetch_assoc($mysqlResult)) { ?>
+        $mysqlResult = query("SELECT * FROM tb_blogpost");
+        while($blogPost = mysqli_fetch_assoc($mysqlResult)) { ?>
                     
-                        <span class="post-title"> 
-                            <?php echo $blogPost['title']; ?> 
-                        </span>
+        <span class="post-title"> 
+            <?php echo $blogPost['title']; ?> 
+        </span>
                     
-                        <div class="post-content">
-                            <p>
-                                <?php echo $blogPost['content']; ?>
-                            </p>
-                        </div>
-                    
-                    <?php } ?>
-                </div>
-                
-            </div>
-            
+        <div class="post-content">
+            <p>
+                <?php echo $blogPost['content']; ?>
+            </p>
         </div>
-        
-        <div id="footer"></div>
-    </body>
-</html>
+                    
+        <?php } ?>
+    </div>
+                
+</div>
+            
+<?php include ('./templates/footer.php'); ?>
