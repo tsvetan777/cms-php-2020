2884b8dd341129a5656fe27dd672ce0624781d75
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-12 20:14:00 +0300

    Add extra methods for returning the last inserted ID. The database connection is synchronised

--- src/db/database.php
+++ src/db/database.php
@@ -1,11 +1,24 @@
 <?php
 
+function dbConnect() {
+    
+    if(isset($_SESSION['is_connected_to_db'])) {
+        return $_SESSION['is_connected_to_db'];
+    }
+    
+    // CREATE AND STORE CONNECTION TO DATABASE:
+    
+    $_SESSION['is_connected_to_db'] = mysqli_connect("localhost", "root", "", "mycms");
+    return $_SESSION['is_connected_to_db'];
+    
+}
+
 // SELECT * FROM tb_blogpost
 function query($query) {      
     
     // Step 1: Connect to database
     
-   $connection = mysqli_connect("localhost", "root", "", "mycms");
+    $connection = dbConnect();
     
     if(!$connection) {
         echo mysqli_connect_error();
@@ -16,11 +29,22 @@ function query($query) {
     
     if(!$databaseResult) {
         
-        echo '<div class="db-error>"';
+        echo '<div class="db-error">';
         echo mysqli_error($connection);
-        
+        echo '</div>';
     }
     
     return $databaseResult;
 }
 
+/**
+ * This function returns last id
+ * @name $getLastInsertedId
+ * @author Tsvetan Tachev
+ * @return type
+ */
+
+function getLastInsertedId() {
+    
+    return mysqli_insert_id(dbConnect());
+}
