5805585405f344a984b6cd5af8dd382c70b44bf2
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-12 20:14:17 +0300

    Add new database objects

---
+++ sql/migration_10_07_2020_sql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_users (
+    id 			INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name		VARCHAR(256),
+    fname		VARCHAR(256),
+    lname		VARCHAR(256),
+    email		VARCHAR(256),
+    password	VARCHAR(256)
+)
+
+SELECT * FROM tb_users;
