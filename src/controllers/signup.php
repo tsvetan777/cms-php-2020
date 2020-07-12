9d37cd9dd427d1693df996a34e7d9c3916b2f026
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-12 20:14:59 +0300

    Modify the process of authorizing new user

--- src/controllers/signup.php
+++ src/controllers/signup.php
@@ -1,5 +1,42 @@
 <?php
 
+function isUserAlreadyExists($columnName) {
+    
+    $userName   = $columnName['user_name'];
+    $userEmail  = $columnName['user_email'];
+    
+    $validateIfRegistrationUserAlreadyExistQuery = "SELECT * FROM tb_users WHERE "
+            . "name = '$userName' OR email = '$userEmail'";
+    $databaseQueryRedult    = query($validateIfRegistrationUserAlreadyExistQuery);
+    $requestResult          = mysqli_fetch_assoc($databaseQueryRedult); 
+    
+    return ($requestResult != null);
+}
+
+function createNewUserInDatabase($databaseColumn) {
+    
+    $userName   = $databaseColumn['user_name'];
+    $userFname  = $databaseColumn['user_fname'];
+    $userLname  = $databaseColumn['user_lname'];
+    $userEmail  = $databaseColumn['user_email'];
+    $userPass   = $databaseColumn['user_pass'];
+    
+    $createNewUserRequest   = "INSERT INTO tb_users(name, fname, lname, email, pass)"
+                            . "VALUES('$userName', '$userFname', '$userLname', '$userEmail', '$userPass')";
+    
+    return query($createNewUserRequest);
+}
+
+        // UPDATE:
+function assignRoleToUser($userId, $roleId) {
+    
+    $assignRoleToInsertedUserQuery  = "INSERT INTO tb_user__role(user_id, role_id)"
+                                    . "VALUES($lastInsertedId, $userRoleId)";
+    
+    return query($assignRoleToInsertedUserQuery);
+    
+}
+
 if(isset($_POST['user_request_tokken']) AND $_POST['user_request_tokken'] == 1) {
     
     $userName       = isset($_POST['user_name'])        ? $_POST['user_name']           : '';
@@ -25,19 +62,33 @@ if(isset($_POST['user_request_tokken']) AND $_POST['user_request_tokken'] == 1) 
     if(strlen($userEmail) < 5) {
         return setFormError('signup', 'user_email', 'Min lenght 5 characters is required');
     }
-    
+
     if($userPass != $userPassRepeat) {
         return setFormError('signup', 'user_pass', 'User password and password repeat must be the same string');
     }
     
-    $createNewUserRequest = "INSERT INTO tb_users(name, fname, lname, email, pass)"
-            . "VALUES('$userName', '$userFName', '$userLName', '$userEmail', '$userPass')";
-
-    if (query($createNewUserRequest)) {
-        echo "User created successfully!";
+    if($isUserAlreadyExists(array('user_name' => $userName, 'user_email' => $userEmail))) {
+        return setFormError('signup', 'user_name', 'This user already exists!');
     }
+    
+    $isUserCreated = createNewUserInDatabase(array(
+        'user_name'     => $userName,
+        'user_fname'    => $userFname,
+        'user_lname'    => $userLname,
+        'user_email'    => $userEmail,
+        'user_pass'     => $userPass
+    ));
+    
+    if($isUserCreated) {
+        
+        $isRoleAssignedSuccessfully = assignRoleToUser(getLastInsertedId(), 1);
+    
+        if($isRoleAssignedSuccessfully) {
+           echo 'User created successfully!';
+        }
+    }
+    
 }
-
 
 
 
