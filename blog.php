a0618675aabdfed606219a7cad81cf950f7012fd
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-19 19:59:57 +0300

    Add search for blog post content

--- blog.php
+++ blog.php
@@ -3,28 +3,48 @@
 
 <?php $categoryFetch = Database::query("SELECT * FROM tm_categories"); ?>
 
-
-<ul id="category-placeholder">
+<div class="wrapper">
     
-    <?php while ($blogCategory = Database::fetch(categoryFetch)) { ?>
+    <ul id="category-placeholder" class="mb-25">
+    
+        <?php while ($blogCategory = Database::fetch(categoryFetch)) { ?>
 
-        <li class="category-placeholder--category">
+            <li class="category-placeholder--category">
             
-            <?php $categoryId = $blogCategory['id']; ?>
+                <?php $categoryId = $blogCategory['id']; ?>
             
-            <a href="<?php echo "http://localhost/CMS/blog.php?category=$categoryId" ?> ">
-                <?php echo $blogCategory['title'] ?>
-            </a>
+                <a href="<?php echo "http://localhost/CMS/blog.php?category=$categoryId" ?> ">
+                    <?php echo $blogCategory['title'] ?>
+                </a>
             
-        </li>
+            </li>
         
-        <li class="category-placeholder--category">
-            <a href="http://localhost/CMS/blog.php">Изчисти</a>
-        </li>
+            <li class="category-placeholder--category">
+                <a href="http://localhost/CMS/blog.php">Изчисти</a>
+            </li>
     
-     <?php } ?>
+        <?php } ?>
     
-</ul>
+    </ul>
+    
+    <div>
+        <form method="GET">
+            
+            <input type="text" placeholder="Търси ..." name="q">
+            
+             <select name="q_selector">
+                <option value="title">Търси по заглавие</option>
+                <option value="content">Търси по съдържание</option>
+            </select>
+            
+            <input type="hidden" name="post_search_tokken" value="1">
+            <input type="submit">
+            
+        </form>
+    </div>
+    
+</div>
+
             
 <div id="blog-post--content">
                 
--- src/controllers/blog.php
+++ src/controllers/blog.php
@@ -8,6 +8,24 @@ if(Auth::isAuthenticated()) {
 function listAllBlogPost() {
     
     $myCategory = null;
+    
+    if(isset($_GET['post_search_tokken']) && $_GET['post_search_tokken'] == 1) {
+    
+        $searchQuery            = $_GET['q'];
+        $searchDescriptorColumn = $_GET['q_selector'];
+        
+        $categoryQuery          = $myCategory ? "b.category_id = $myCategory AND" : "";
+    
+        // LIKE оператора match-ва всички резултати от базата данни, 
+        // в които се съдържа въведен от нас input
+        $requestQuery = "SELECT * FROM tb_blogpost a, tb_blog_post__categories b"
+                      . "WHERE a.id = b.blog_post_id AND b.category_id = $myCategory"
+                      . "AND a.$searchDescriptorColumn LIKE '%$searchQuery%'";
+        
+    
+        return Database::query($requestQuery);
+    }
+    
     if(isset($_GET['category'])) {
         
         $myCategory = $_GET['category'];
@@ -22,3 +40,4 @@ function listAllBlogCategory() {
     
     return Database::query("SELECT * FROM tm_categories");
 }
+
