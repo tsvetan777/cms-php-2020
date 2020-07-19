cc83ebe46ed02a9f35170d27482ca4625c5db45f
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-19 20:51:24 +0300

    Extend authentication with user and role information

--- src/controllers/signin.php
+++ src/controllers/signin.php
@@ -5,14 +5,25 @@ if(isset($_POST['user_request_tokken']) && $_POST['user_request_tokken'] == 1) {
     $userEmail      = $_POST['user_email'];
     $userPassword   = $_POST['user_pass'];
 
-    $checkIfUserExistsQuery  = "SELECT * FROM tb_users WHERE "
-                             . "email = '$userEmail' AND password = '$userPassword'";
+    $checkIfUserExistsQuery  = " SELECT * FROM tb_users WHERE "
+                             . " email = '$userEmail' AND password = '$userPassword' ";
 
     $userRecord = Database::get($checkIfUserExistsQuery);
     
     if($userRecord) {
-        //$_SESSION['is_authenticated'] = true;
-        Auth::setAuthenticationFlagToAvailable();
+        
+        $userRoleId                     = $userRecord['id'];
+        $getUserRoleCollectionQuery     = " SELECT b.ttitle AS role_title "
+                                        . " FROM tb_user__role AS a, "
+                                        . " WHERE user_id = $userRoleId "
+                                        . " AND a.role_id = b.id ";
+        $userRoleCollection             = Database::getall($getUserRoleCollectionQuery);
+        
+        Auth::setAuthenticatedUser(array(
+            'user_data_collection' => $userRecord,
+            'user_role_collection' => $userRoleCollection
+        ));
+        
         return redirectTo('index');
     }
     
