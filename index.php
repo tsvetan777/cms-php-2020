13b72b9eb2c247f19dce1557ca7469b2a8006ae7
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-07-19 14:48:50 +0300

    Signin controller created

--- index.php
+++ index.php
@@ -1,32 +1,5 @@
 <?php include ('./templates/header.php'); ?>
 
-<ul id="category-placeholder">
-    <li class="category-placeholder--category">Клюки</li>
-    <li class="category-placeholder--category">Политика</li>
-    <li class="category-placeholder--category">Програмиране</li>
-</ul>
-            
-<div id="blog-post--content">
-                
-    <div class="post">
-        <?php 
-                    
-        $mysqlResult = Database::query("SELECT * FROM tb_blogpost");
-        while($blogPost = Database::fetch($mysqlResult)) { ?>
-                    
-        <span class="post-title"> 
-            <?php echo $blogPost['title']; ?> 
-        </span>
-                    
-        <div class="post-content">
-            
-            <p><?php echo $blogPost['content']; ?></p>
-            
-        </div>
-                    
-        <?php } ?>
-    </div>
-                
-</div>
+    <h2>Welcome to Custom CMS</h2>
             
 <?php include ('./templates/footer.php'); ?>
