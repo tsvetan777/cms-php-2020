01af26de5479d3e2e21e329ba7a759ad36df2e0c
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-09-10 00:15:46 +0300

    Add new separate pages for creation of blog post

---
+++ admin_post.php
@@ -0,0 +1,24 @@
+<?php include ('./templates/admin/header.php'); ?>
+<?php include ('./src/util/static_list.php'); ?>
+<?php include ('./src/controllers/admin.php'); ?>
+
+<div class="wrapper">
+    
+    <form method="POST">
+        
+        <input type="text" name="post_title" placeholder="Post title">
+        
+        <?php getCategoryStaticList('post_category'); ?>
+        
+        <textarea name="post_content" placeholder="Post content"></textarea>
+        <textarea name="post_preview_content" placeholder="Post preview content"></textarea>
+            
+        <input type="hidden" name="create_new_post_tokken" value="1">
+        <input type="submit" value="Създай нов пост">
+            
+    </form>
+    
+</div>
+
+
+<?php include ('./templates/admin/footer.php'); ?>
