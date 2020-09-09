725795fe4e03a4775077599342f17bbf4c1fb953
tsvetan777 <tsvetan.borisov77@gmail.com>
2020-09-10 00:13:55 +0300

    Separate user header and footer from the administration one

---
+++ templates/admin/header.php
@@ -0,0 +1,42 @@
+<?php session_start(); ?>
+<?php include './src/db/Database.php';    ?>
+<?php include './src/models/Auth.php';          ?>
+<?php include './src/util/form.php';            ?>
+<?php include './src/util/redirect.php';        ?>
+
+<html>
+    <head>
+        <title>title</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    
+    <body>
+        <div id="header">
+            
+            
+            <h1 class="logo">@CMS Admin</h1>
+            
+            <div id="cms-manager">
+                
+                <?php if(Auth::isAuthenticated()) { ?>
+                    <ul>
+                        <li class="list-item"><a href="signout.php">Sign out</a></li>
+                    </ul>
+                <?php } ?>
+                
+                <?php if(Auth::isModerator() || Auth::isAdmin()) { ?>
+                    <ul>
+                        <li class="list-item"><a href="admin_post.php">Admin post management</a></li>
+                    </ul>
+                <?php } ?>
+                
+                <?php if(Auth::isAdmin()) { ?>
+                    <ul>
+                        <li class="list-item"><a href="admin_category.php">Admin category</a></li>
+                    </ul>
+                <?php } ?>
+                       
+            </div>
+        </div>
+        
+        <div id="content">
